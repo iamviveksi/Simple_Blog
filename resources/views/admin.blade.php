2cca2b8603fa817fee917868fa385dd7cdc0ee14
vivek singh <iamviveksi@gmail.com>
2021-12-04 12:45:39 +0545

    Updated

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Laravel-Admin</title>
+    <title>Simple Blog Admin</title>
     <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
 </head>
 <body>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Laravel-Admin</title>
+    <title>Simple Blog</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
